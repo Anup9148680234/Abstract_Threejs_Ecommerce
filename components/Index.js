d9424c3fe3fc1132fe734271b174e0a5381c14b3
Anup Angadi <anup.angadi@hbwsl.com>
2024-05-07 17:55:26 +0530

    Created a Function to Fetch Product Detail JSON and Render 3D Models based on the data Fetched

--- components/Index.js
+++ components/Index.js
@@ -5,35 +5,49 @@ import Products from "./ProductsList";
 import { abstractRender } from "./Abstract"
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as products_details from './products.json'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+
 
 const prodDetials = products_details.default.products;
-function Render3DProducts( modelPath , canvasElement , cameraPositionZ ) { 
-  const scene = new THREE.Scene()
-  scene.name = "Products";
-  const sizes = { width: 300 , height: 300 }
-  const loader = new GLTFLoader();
 
+const sizes = { width: 300 , height: 300 };
+
+const lights = new THREE.AmbientLight( 0xffffff , 1 );
+function Render3DProducts( modelPath , canvasElement , cameraPositionZ , cameraPositionY, scale) { 
+  const scene = new THREE.Scene()
+  const loader = new GLTFLoader();
   loader.load( modelPath, function ( gltf ) {
-    //gltf.scene.scale.set(3, 3, 3);  
-      scene.add( gltf.scene );
+    console.log(gltf.scene)
+    gltf.scene.scale.set(scale?scale:1, scale?scale:1, scale?scale:1); 
+    gltf.scene.translateY(cameraPositionY ? cameraPositionY :0);
+    scene.add( gltf.scene );
   }, undefined, function ( error ) {
       console.error( error );
   } );
 
-  scene.add( new THREE.AmbientLight( 0xffffff , 1 ) );
-
+  scene.add(lights);
   const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-  camera.position.z = cameraPositionZ
+  camera.position.set(0, 0, cameraPositionZ);
   scene.add(camera)
 
   const canvas = document.querySelector(canvasElement)
   const renderer = new THREE.WebGLRenderer({
-      canvas: canvas
+    canvas: canvas,
+    antialias: true
   })
   renderer.setClearColor( 0x000000 , 0);
   renderer.setSize(sizes.width, sizes.height)
   renderer.render(scene, camera)
+  renderer.toneMapping = THREE.EquirectangularReflectionMapping;
 
+  const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+  const hdriLoader = new RGBELoader()
+  hdriLoader.load( 'public/ox_bridge_morning_1k.hdr', function ( texture ) {
+    const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+    texture.dispose(); 
+    scene.environment = envMap
+  } );
 
   const controls = new OrbitControls(camera,canvas);
   controls.enableDamping = true;
@@ -41,13 +55,6 @@ function Render3DProducts( modelPath , canvasElement , cameraPositionZ ) {
   controls.enableZoom = false;
   controls.autoRotate = true;
   controls.autoRotateSpeed = 1;
-
-  window.addEventListener('resize', ()=>{
-      location.replace(location.href);
-      camera.updateProjectionMatrix();
-      camera.aspect = sizes.width/ sizes.height;
-      renderer.setSize(sizes.width, sizes.height);
-  });
   
   const loop = () =>{
       controls.update();
@@ -57,7 +64,6 @@ function Render3DProducts( modelPath , canvasElement , cameraPositionZ ) {
   loop();
 }
 
-console.log("Index Reached")
 Navbar();
 Products();
 abstractRender();
@@ -65,5 +71,10 @@ abstractRender();
 
 for (let index = 0; index < prodDetials.length; index++) {
     const element = prodDetials[index];
-    Render3DProducts(element.modelpath, element.canvas, element.cameraPositionZ)
+    Render3DProducts(
+      element.modelpath, 
+      element.canvas, 
+      element.cameraPositionZ, 
+      element.cameraPositionY,
+      element.scale)
 }
--- components/ProductsList.js
+++ components/ProductsList.js
@@ -1,103 +1,68 @@
 export default function Products(){
+  let products = "";
+  let dict = [
+    {
+      "name": "Samsung Galaxy S22 Ultra",
+      "price": "$899"
+    },
+    {
+      "name": "PS5 Controller",
+      "price": "$499"
+    },{
+      "name": "Sauvage Perfume",
+      "price": "$99"
+    },{
+      "name": "Apple Watch Ultra 2",
+      "price": "$799"
+    },{
+      "name": "Apple iPhone 13 pro max",
+      "price": "$899"
+    },{
+      "name": "Apple MacBook Pro M3 ",
+      "price": "$1299"
+    },{
+      "name": "Blue Vans Shoes",
+      "price": "$69"
+    },{
+      "name": "Dyson Supersonic Hair Dryer",
+      "price": "$499"
+    },{
+      "name": "Shiba Inu Action Figure",
+      "price": "$48"
+    },{
+      "name": "Shiba Inu Action Figure",
+      "price": "$48"
+    },{
+      "name": "Shiba Inu Action Figure",
+      "price": "$48"
+    },{
+      "name": "Shiba Inu Action Figure",
+      "price": "$48"
+    }
+  ]
+  for(let i=0; i<12 ; i++){
+    products += `<a href="#" class="group" draggable="false">
+                  <div class="flex w-full overflow-hidden rounded-lg bg-gray-200 justify-center">
+                    <canvas class="prod${i+1}"></canvas>
+                  </div>
+                  <h3 class="mt-4 text-sm text-gray-700">${dict[i].name}</h3>
+                  <p class="mt-1 text-lg font-medium text-gray-900">${dict[i].price}</p>
+                </a>`
+  }
+
     document.querySelector('#product').innerHTML = `
     <div class="bg-white  h-full overflow-auto ">
     <div class="mx-auto max-w-2xl px-4  sm:px-6  lg:max-w-7xl lg:px-8">
       <h2 class=" text-2xl font-semibold p-10 ">Products</h2>
-      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="">
-        
-        <a href="#" class="group" draggable="false">
-          <div class="flex w-full overflow-hidden rounded-lg bg-gray-200 ">
-            <canvas class="prod1"></canvas>
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Shiba Inu Action Figure</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-        </a>
-
-        <a href="#" class="group" draggable="false">
-          <div class="flex w-full overflow-hidden rounded-lg bg-gray-200 ">
-            <canvas class="prod2"></canvas>
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Shiba Inu Action Figure</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-        </a>
-
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-        </a>
-
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg" alt="Olive drab green insulated bottle with flared screw lid and flat top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg" alt="Person using a pen to cross a task off a productivity paper card." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-        </a>
-        <a href="#" class="group">
-          <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="h-full w-full object-cover object-center group-hover:opacity-75">
-          </div>
-          <h3 class="mt-4 text-sm text-gray-700">Machined Mechanical Pencil</h3>
-          <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-        </a>
-  
-        <!-- More products... -->
+      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" id="prodGird">
+        ` + 
+          products
+        + `
       </div>
     </div>
   </div>
     `
+
 }
+
+
